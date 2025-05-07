4653f979f9883fe548625a5d44186ef198e88026
Jessica Yuen <jess986.codes@gmail.com>
2025-05-07 20:31:57 +1000

    refactor class names

--- event-management-backend/src/test/java/com/woffles/event_management_backend/EventManagerApplicationTests.java
+++ event-management-backend/src/test/java/com/woffles/event_management_backend/EventManagerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EventManagementBackendApplicationTests {
+class EventManagerApplicationTests {
 
 	@Test
 	void contextLoads() {
